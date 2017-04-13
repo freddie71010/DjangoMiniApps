85d6b7d6c62e161d40f64a06174256f208f40bc9
Freddie71010 <freddie71010@gmail.com>
2017-04-13 17:04:19 -0400

    set up setting and urls for new todo app and static folder

--- projectDP/projectDP/urls.py
+++ projectDP/projectDP/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 urlpatterns = [
 	url(r'^', include('polls.urls')),
     url(r'^polls/', include('polls.urls')),
+    url(r'^todos/', include('todos.urls')),
     url(r'^admin/', admin.site.urls),
 ]
