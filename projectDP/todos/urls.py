f34d8b760f36b051d23b8c393cb796ec7d942c39
Freddie71010 <freddie71010@gmail.com>
2017-04-13 17:03:28 -0400

    create new Todo app - dragging and dropping  to do list items via jquery UI

---
+++ projectDP/todos/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'todos'
+
+# Generic template URLs
+# ================================================================================
+urlpatterns = [
+	# ex: /todos/
+	url(r'^$', views.IndexView.as_view(), name='index'),
+]
