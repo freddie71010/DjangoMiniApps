b37bb360887b92a689103b3b78812cb1cacad834
Freddie71010 <freddie71010@gmail.com>
2017-04-14 11:05:10 -0400

    update models.py with new 'section' field (WIP, Completed, Future)

--- projectDP/todos/models.py
+++ projectDP/todos/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 
+
+SECTIONS =  (
+		('WIP', 'WIP'),
+		('Completed', 'Completed'),
+		('Future', 'Future'),
+	)
+
 class Todo(models.Model):
 	task = models.CharField(max_length=200)
 	task_row_order = models.IntegerField()
-
+	section = models.CharField(max_length=10, choices=SECTIONS)
+	
 	def __str__(self):
 		return self.task
+
+	
