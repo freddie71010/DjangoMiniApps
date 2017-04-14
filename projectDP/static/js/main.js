dbf8aeb993bb8bdf0870c481f67e06fa31c578ff
Freddie71010 <freddie71010@gmail.com>
2017-04-14 11:04:26 -0400

    Drag and drop list successfully saves correct order to DB on button click

--- projectDP/static/js/main.js
+++ projectDP/static/js/main.js
@@ -1,8 +1,14 @@
 $(document).ready(function(){
 	console.log("main.js loaded!")
 	$( function() {
-		$("#sortable").sortable();
-		$("#sortable").disableSelection();
+		$("#sortable1").sortable();
+		$("#sortable1").disableSelection();
+	} );
+
+	$( function() {
+		$( "#sortable1, #sortable2" ).sortable({
+			connectWith: ".connectedSortable"
+		}).disableSelection();
 	} );
 
 	$('#update_list').on('click', function(event){
@@ -10,7 +16,24 @@ $(document).ready(function(){
 		console.log("clicked 'Update List Order' button!");
 		var sorted = $("#sortable").sortable("toArray");
 		console.log(sorted);
+		kwargs = {
+			"sorted": sorted,
+			"csrfmiddlewaretoken": $('input[name="csrfmiddlewaretoken"]').val(),
+		}
+	
+		$.ajax({
+			url: "",
+			data: kwargs,
+			type: "POST",
+			success: function(response){
+				console.log("AJAX success!");
+			},
+			error: function(){
+				console.log("Error in AJAX")
+			}
+		}) //end ajax
+	
 
-	});
+	}); //end update_list button
 
 }); //end doc
--- projectDP/todos/views.py
+++ projectDP/todos/views.py
@@ -6,9 +6,27 @@ class IndexView(View):
 	template = "todos/index.html"
 	
 	def get(self, request):
-		todos = Todo.objects.all()
+		todos = Todo.objects.all().order_by('task_row_order')
 		print(todos)
 		return render(request, self.template, {'todos': todos})
 
 	def post(self, request):
-		pass
+		new_row_order = dict(request.POST)
+		new_row_order = new_row_order["sorted[]"]
+		
+		# strips down row order to just an integer
+		for i in range(len(new_row_order)):
+			new_row_order[i]= int(new_row_order[i].replace("item_",""))
+		
+		todos = Todo.objects.all().order_by('task_row_order')
+		i = 0
+		# iterates through todos and updates todo task_row_order with new value
+		for todo in todos:
+			updated_row_num = new_row_order.index(todo.task_row_order)
+			print(todo,end="\t\t")
+			print("updated_row_num: ", updated_row_num)
+			todo.task_row_order = updated_row_num
+			todo.save(update_fields=['task_row_order'])
+			i += 1
+
+		return render(request, self.template, {'todos': todos})
